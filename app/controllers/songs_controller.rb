edfe63baf7c62c1d88b330fae719fdb0d3ee9575
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-26 04:46:13 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
   end
 
   def show
-    if Song.find(params[:id]) == nil
+    if Song.find_by(id: params[:id]) == nil
       flash[:alert] = "Song not found."
       redirect_to artist_songs_path(params[:artist_id])
     else
